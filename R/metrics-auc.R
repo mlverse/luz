2767c0f926fd666e3332d8b44728e4ee9dc3a1b0
Daniel Falbel <dfalbel@gmail.com>
2021-05-20 17:28:28 -0300

    basic implementation of multiclass AUC

--- R/metrics-auc.R
+++ R/metrics-auc.R
@@ -1,6 +1,39 @@
 #' @include metrics.R
+NULL
 
-#' @title Computes the area under the ROC
+luz_metric_auc_base <- luz_metric(
+  initialize = function(num_thresholds=200,
+                        thresholds=NULL,
+                        from_logits=FALSE) {
+    if (!is.null(thresholds)) {
+      self$num_thresholds <- length(thresholds) + 2
+      thresholds <- sort(thresholds)
+    } else {
+      if (num_thresholds <= 1)
+        rlang::abort("num_thresholds must be > 1")
+
+      # Otherwise, linearly interpolate (num_thresholds - 2) thresholds in
+      # (0, 1).
+      self$num_thresholds <- num_thresholds
+      thresholds = (2:(num_thresholds-1))/(num_thresholds-2)
+    }
+
+    # Add an endpoint "threshold" below zero and above one for either
+    # threshold method to account for floating point imprecisions.
+    eps <- torch::torch_finfo(torch::torch_float32())$eps
+    self$thresholds = torch::torch_tensor(c(0.0 - eps, thresholds, 1.0 + eps))
+
+    self$from_logits <- from_logits
+
+    # Initialize state
+    self$initialize_state()
+  },
+  initialize_state = function() {
+    rlang::abort("not implemented")
+  }
+)
+
+#' Computes the area under the ROC
 #'
 #' To avoid storing all predictions and targets for an epoch we compute confusion
 #' matrices across a range of pre-established thresholds.
@@ -17,32 +50,8 @@
 #' @export
 luz_metric_binary_auroc <- luz_metric(
   abbrev = "AUC",
-  initialize = function(num_thresholds=200,
-                        thresholds=NULL,
-                        from_logits=FALSE) {
-
-
-    if (!is.null(thresholds)) {
-      self$num_thresholds <- length(thresholds) + 2
-      thresholds <- sort(thresholds)
-    } else {
-      if (num_thresholds <= 1)
-        rlang::abort("num_thresholds must be > 1")
-
-      # Otherwise, linearly interpolate (num_thresholds - 2) thresholds in
-      # (0, 1).
-      self$num_thresholds <- num_thresholds
-      thresholds = (1:(num_thresholds-1))/(num_thresholds-1)
-    }
-
-    # Add an endpoint "threshold" below zero and above one for either
-    # threshold method to account for floating point imprecisions.
-    eps <- torch::torch_finfo(torch::torch_float32())$eps
-    self$thresholds = torch::torch_tensor(c(0.0 - eps, thresholds, 1.0 + eps))
-
-    self$from_logits <- from_logits
-
-    # Initialize state
+  inherit = luz_metric_auc_base,
+  initialize_state = function() {
     self$true_positives <- torch::torch_zeros(size = self$num_thresholds)
     self$false_positives <- torch::torch_zeros(size = self$num_thresholds)
     self$n_pos <- torch::torch_zeros(size = 1)
@@ -73,6 +82,57 @@ luz_metric_binary_auroc <- luz_metric(
     mult <- torch:::torch_diff(fpr, n=1, prepend = 0)
 
     torch_sum(tpr*mult)$item()
+  }
+)
+
+luz_metric_multiclass_auc <- luz_metric(
+  abbrev = "AUC",
+  inherit = luz_metric_auc_base,
+  initialize_state = function(num_classes) {
+
+    if (missing(num_classes))
+      return()
+
+    self$num_classes <- num_classes
+    size <- c(self$num_classes, self$num_thresholds)
+
+    self$true_positives <- torch::torch_zeros(size = size)
+    self$false_positives <- torch::torch_zeros(size = size)
+    self$n_pos <- torch::torch_zeros(size = self$num_classes)
+    self$n_neg <- torch::torch_zeros(size = self$num_classes)
+  },
+  update = function(preds, targets) {
+
+    # initialize state. we expect one column for each class
+    if (is.null(self$num_classes))
+      self$initialize_state(preds$shape[2])
+
+    if (self$from_logits)
+      preds <- torch::nnf_softmax(preds)
+
+    # targets are expected to be integers representing the classes
+    # so we one hot encode them
+    targets <- nnf_one_hot(targets, num_classes = self$num_classes)
+
+    # usqueeze dims
+    preds <- preds$unsqueeze(3)
+    targets <- targets$unsqueeze(3)
+
+    predictions_per_thresh <- preds > self$thresholds
+    comparisons <- predictions_per_thresh == targets
+
+    self$true_positives$add_(torch_sum(comparisons * (targets == 1), dim = 1))
+    self$false_positives$add_(torch_sum(comparisons * (targets == 0), dim = 1))
+    self$n_pos$add_(torch_sum(targets == 1, dim = 1)$squeeze(2))
+    self$n_neg$add_(torch_sum(targets == 0, dim = 1)$squeeze(2))
+  },
+  compute = function() {
+
+    tpr <- self$true_positives/self$n_pos$unsqueeze(2)
+    fpr <- self$false_positives/self$n_neg$unsqueeze(2)
+    mult <- torch::torch_diff(fpr, n=1, prepend = torch_zeros(2,1), dim = 2)
+
+    torch_mean(torch_sum(tpr*mult,dim=2))$item()
   }
 )
 
--- tests/testthat/test-metrics-auc.R
+++ tests/testthat/test-metrics-auc.R
@@ -19,3 +19,29 @@ test_that("binary auc works", {
   )
 
 })
+
+test_that("multiclass auc works", {
+
+  actual <- c(1, 1, 1, 0, 0, 0) + 1L
+  predicted <- c(0.9, 0.8, 0.4, 0.5, 0.3, 0.2)
+  predicted <- cbind(1-predicted, predicted)
+
+  y_true <- torch_tensor(as.integer(actual))
+  y_pred <- torch_tensor(predicted)
+
+  m <- luz_metric_multiclass_auc(num_thresholds = 1e5)
+  m <- m$new()
+
+  m$update(y_pred[1:2,], y_true[1:2])
+  m$update(y_pred[3:4,], y_true[3:4])
+  m$update(y_pred[5:6,], y_true[5:6])
+
+  expect_equal(
+    m$compute(),
+    mean(c(
+      Metrics::auc(as.numeric(actual == 1), predicted[,1]),
+      Metrics::auc(as.numeric(actual == 2), predicted[,2])
+    ))
+  )
+
+})
