3ed0141c1fbe31a66ac97549b39e4c10dce3aefe
Daniel Falbel <dfalbel@gmail.com>
2021-06-14 16:08:18 -0300

    add more documentation and includethe total batch time

--- R/callbacks-profile.R
+++ R/callbacks-profile.R
@@ -2,6 +2,17 @@
 #'
 #' Computes the times for high-level operations in the training loops.
 #'
+#' @details
+#' Records are saved in `ctx$records$profile`. Times are stored as seconds.
+#' Data is stored in the following structure:
+#'
+#' - **fit** time for the entire fit procedure.
+#' - **epoch** times per epoch
+#' - **(train/valid)_batch** time per batch of data processed, including data
+#'   acquisition and step.
+#' - **(train/valid)_batch** time per step (training or validation step) - only
+#'   the model step. (not including data acquisition and preprocessing)
+#'
 #' @note In general you don't need to use these callback by yourself because it's always
 #'   included by default in [fit.luz_module_generator()].
 #'
@@ -29,30 +40,38 @@ luz_callback_profile <- luz_callback(
 
   on_train_begin = function() {
     self$tic("train")
+    self$tic("train_batch")
   },
   on_train_end = function() {
+    trash <- self$toc("train_batch") # we don't use the last one, but we still reset it.
     ctx$log("profile", "train", self$toc("train"))
   },
 
   on_train_batch_begin = function() {
-    self$tic("train_batch")
+    self$tic("train_step")
   },
   on_train_batch_end = function() {
+    ctx$log("profile", "train_step", self$toc("train_step"))
     ctx$log("profile", "train_batch", self$toc("train_batch"))
+    self$tic("train_batch")
   },
 
   on_valid_begin = function() {
     self$tic("valid")
+    self$tic("valid_batch")
   },
   on_valid_end = function() {
+    trash <- self$toc("valid_batch")
     ctx$log("profile", "valid", self$toc("valid"))
   },
 
   on_valid_batch_begin = function() {
-    self$tic("valid_batch")
+    self$tic("valid_step")
   },
   on_valid_batch_end = function() {
+    ctx$log("profile", "valid_step", self$toc("valid_step"))
     ctx$log("profile", "valid_batch", self$toc("valid_batch"))
+    self$tic("valid_batch")
   },
 
   tic = function(name) {
@@ -61,6 +80,8 @@ luz_callback_profile <- luz_callback(
     self$tics[[name]] <- Sys.time()
   },
   toc = function(name) {
+    if (is.null(self$tics[[name]])) return()
+
     time <- difftime(Sys.time(), self$tics[[name]], units = "secs")
     self$tics[[name]] <- NULL
     as.numeric(time)
--- tests/testthat/test-callbacks-profile.R
+++ tests/testthat/test-callbacks-profile.R
@@ -11,11 +11,17 @@ test_that("callback profile", {
 
   output <- mod %>%
     set_hparams(input_size = 10, output_size = 1) %>%
-    fit(dl, verbose = FALSE, epochs = 5)
+    fit(dl, verbose = FALSE, epochs = 5, valid_data = dl)
 
   expect_length(output$ctx$records$profile$fit, 1)
-  expect_length(output$ctx$records$profile$train, 5)
-  expect_length(output$ctx$records$profile$train_batch, 50)
   expect_length(output$ctx$records$profile$epoch, 5)
+
+  expect_length(output$ctx$records$profile$train, 5)
+  expect_length(output$ctx$records$profile$train_step, 50)
+  expect_length(output$ctx$records$profile$train_batch, 50)
+
+  expect_length(output$ctx$records$profile$valid, 5)
+  expect_length(output$ctx$records$profile$valid_step, 50)
+  expect_length(output$ctx$records$profile$valid_batch, 50)
 
 })
