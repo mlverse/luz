dd941948e9946dd86f45d263dff0c381ba6fb2ae
Daniel Falbel <dfalbel@gmail.com>
2021-08-10 10:50:56 -0300

    add a test case

---
+++ tests/testthat/test-lr-finder.R
@@ -0,0 +1,20 @@
+test_that("lr_finder works", {
+
+  dl <- get_dl()
+  model <- get_model()
+  model <- model %>%
+    setup(
+      loss = torch::nn_mse_loss(),
+      optimizer = torch::optim_adam
+    ) %>%
+    set_hparams(input_size = 10, output_size = 1)
+
+  records <- lr_finder(model, dl, verbose = FALSE)
+  expect_s3_class(records, "lr_records")
+  expect_s3_class(records, "data.frame")
+
+  expect_output(print(records))
+
+  p <- plot(records)
+  expect_s3_class(p, "gg")
+})
