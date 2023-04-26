428e5f00f5b238bdcd9b0b99fa001fc9e84f2b0f
Daniel Falbel <dfalbel@gmail.com>
2023-04-26 11:31:56 +0200

    Add callback to use automatic mixed precision.

---
+++ R/callbacks-amp.R
@@ -0,0 +1,40 @@
+#' @include callbacks.R
+NULL
+
+#' Automatic Mixed Precision callback
+#'
+#' This callback will enable [torch::local_autocast()] training model forward
+#' and during loss computation. It will then disable autocast and scale the loss
+#' before `backward()` and `opt$step()`. See [here](https://torch.mlverse.org/docs/articles/amp.html)
+#' for more information.
+#'
+#' @param ... Passed to [torch::cuda_amp_grad_scaler()].
+#'
+#' @returns
+#' A `luz_callback`
+#'
+#' @family luz_callbacks
+#'
+#' @export
+luz_callback_mixed_precision <- luz_callback(
+  "mixed_precision_callback",
+  initialize = function(...) {
+    self$autocast_env <- rlang::new_environment()
+    self$scaler <- torch::cuda_amp_grad_scaler(...)
+  },
+  on_fit_begin = function() {
+    ctx$step_opt <- function(opt) {
+      self$scaler$step(opt)
+      self$scaler$update()
+    }
+  },
+  on_train_batch_begin = function() {
+    torch::local_autocast(device_type = ctx$device, .env = self$autocast_env)
+  },
+  on_train_batch_after_loss = function() {
+    withr::deferred_run(self$autocast_env)
+  },
+  on_train_batch_before_backward = function() {
+    ctx$loss_grad <- self$scaler$scale(ctx$loss_grad)
+  }
+)
---
+++ tests/testthat/test-callbacks-amp.R
@@ -0,0 +1,36 @@
+test_that("Can use mixed precision callback", {
+
+  x <- torch_randn(1000, 10)
+  y <- torch_randn(1000, 1)
+
+  module <- nn_linear
+
+  model <- module %>%
+    setup(loss = nnf_mse_loss, optimizer = optim_adam) %>%
+    set_hparams(in_features = 10, out_features = 1) %>%
+    set_opt_hparams(lr = 1e-4)
+
+  callback_for_testing <- luz_callback(
+    on_fit_begin = function() {
+      expect_true(!identical(ctx$step_opt, default_step_opt))
+    },
+    on_train_batch_begin = function() {
+      if (ctx$iter == 1 && ctx$epoch == 1) {
+        y <- torch_matmul(x, x$t())
+        expect_equal(y$dtype$.type(), "BFloat16")
+      }
+    },
+    on_train_batch_before_backward = function() {
+      if (ctx$iter == 1 && ctx$epoch == 1) {
+        y <- torch_matmul(x, x$t())
+        expect_equal(y$dtype$.type(), "Float")
+      }
+    }
+  )
+
+  fitted <- model %>% fit(list(x, y), valid_data = 0.2, callbacks = list(
+    luz_callback_mixed_precision(enabled = cuda_is_available()),
+    callback_for_testing()
+  ), accelerator = accelerator(cpu = !cuda_is_available()), verbose = FALSE)
+
+})
